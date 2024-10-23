a45723494b6d073614cbf65823ae59a7b80c0f16
Daniel Zdancewicz <inwazzin@gmail.com>
2024-10-23 23:09:24 +0200

    add jsr

--- scripts/build-packages.ts
+++ scripts/build-packages.ts
@@ -1,6 +1,4 @@
-import * as vite from "npm:vite";
-import liba from "../workspace/libs/lib-a/vite.config.ts";
-
-liba.build.lib.entry = "../workspace/libs/lib-a/main.ts";
-console.log(liba);
-vite.build(liba);
+Deno.run({
+  cmd: ["deno", "task", "build"],
+  cwd: "../",
+});
